f2dc57a1b17a586168d834554e38c76ad26b6c0a
Roman Maleev <maleev4you@gmail.com>
2023-11-17 15:54:50 +0300

    First commit

---
+++ src/LaravelAdminServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+namespace LuckyRomka\LaravelAdmin;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAdminServiceProvider extends ServiceProvider
+{
+    public function boot(){
+
+    }
+}
