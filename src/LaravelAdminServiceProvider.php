1b8c5bb434b8b1f635a9067066ba4a4586ebc7a6
Roman <maleev4you@gmail.com>
2023-11-17 19:44:38 +0300

    Add routes and resources

--- src/LaravelAdminServiceProvider.php
+++ src/LaravelAdminServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 class LaravelAdminServiceProvider extends ServiceProvider
 {
     public function boot(){
-
+        $this->loadRoutesFrom(__DIR__."/routes/web.php");
+        $this->loadViewsFrom(__DIR__."/resources/views","lradmin");
     }
 }
